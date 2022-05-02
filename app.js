8770cf4ea4a8e5c845fda504564c492eb260ba51
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-02 15:27:37 -0500

    POST Crea un endpoint que se encargue de crear un explorer en app.js

--- app.js
+++ app.js
@@ -8,11 +8,17 @@ const port = 3000
 app.get('/v1/explorers', (req, res) => {
     console.log(`Api Explorers  GET ALL requests ${new Date()}`)
     const explorer1 = {id:  1, name:"Carlo1"}
-    const explorer2 = {id:  2, name:"Carlo1"}
-    const explorer3 = {id:  3, name:"Carlo1"}
-    const explorer4 = {id:  4, name:"Carlo1"}
+    const explorer2 = {id:  2, name:"Carlo2"}
+    const explorer3 = {id:  3, name:"Carlo3"}
+    const explorer4 = {id:  4, name:"Carlo4"}
     const explorers = [explorer1, explorer2, explorer3, explorer4]
     res.status(200).json(explorers)
+})
+// POST Crea un endpoint que se encargue de crear un explorer
+app.post('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers POST request ${new Date()}`)
+    const requestBody = req.body // Parametros de un cliente
+    res.status(201).json({message: "Created"}) 
 })
 //Puerto en que vamos a ver nuestra app
 app.listen(port, () => {

49e9b976330685945cd37ab03fbbee3e12e34695
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-02 15:35:28 -0500

    PUT Crea un endpoint que se encargue de actualizar un exploreen app.js

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ app.post('/v1/explorers', (req, res) => {
     const requestBody = req.body // Parametros de un cliente
     res.status(201).json({message: "Created"}) 
 })
+//PUT Crea un endpoint que se encargue de actualizar un explore
+app.put('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers PUT request ${new Date()}`)
+    console.log(`Update explorer with id ${req.params.id}`)
+    const requestBody = re.body // Parámetros de un cliente 
+    res.status(200).json({message: "Updated!"})
+})
 //Puerto en que vamos a ver nuestra app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
